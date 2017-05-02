8541de2631344833e53a423c7cb16a36bf2d2f77
Allifreyr <Allifreyr@gmail.com>
2017-05-02 16:51:28 +0000

    Visual Studio

---
+++ TeamCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCode.Startup))]
+namespace TeamCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
