2dc31cd5d9f4a90cffe4c532c7c972c25ac6eb92
katrinoh16 <katrinoh16@ru.is>
2017-05-05 11:57:07 +0000

    new classes for Project, File, ProjectServie and FileService

---
+++ TeamCode/Models/Entities/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamCode.Models.Entities
+{
+    public class File
+    {
+        public int id { get; set; }
+        public string fileName { get; set; }
+        public int projectId { get; set;  }
+
+        //Vantar inn data, af hvaða týpu á það að vera? (Kata)
+    }
+}
---
+++ TeamCode/Models/Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamCode.Models.Entities
+{
+    public class Project
+    {
+        public int id { get; set; }
+        public string projectName { get; set; }
+    }
+}
