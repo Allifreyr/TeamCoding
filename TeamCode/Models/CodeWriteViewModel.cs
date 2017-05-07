d177bb3c37972ab89cceb9e264ebbff1a5bd1357
hjordisj16 <hjordisj16@ru.is>
2017-05-07 22:36:44 +0000

    Save Button fyrir Editor Alexandra og Anna

---
+++ TeamCode/Models/CodeWriteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamCode.Models
+{
+    public class CodeWriteViewModel
+    {
+        public string Content { get; set; }
+    }
+}
