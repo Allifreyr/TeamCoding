da9f19bd633f6fe233d0ea7b84a7cf54afb4ed3e
katrinoh16 <katrinoh16@ru.is>
2017-05-07 22:10:42 +0000

    Connect ProjectId with File

--- TeamCode/Controllers/MyFilesController.cs
+++ TeamCode/Controllers/MyFilesController.cs
@@ -6,24 +6,39 @@ using System.Web.Mvc;
 using TeamCode.Services;
 using TeamCode.Models.Entities;
 using Microsoft.AspNet.Identity;
+using System.Net;
 
 namespace TeamCode.Controllers
 {
     public class MyFilesController : Controller
     {
-        
         // GET: MyFiles
         [Authorize]
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var f = FileService.Instance.GetFilesByProject(id.Value);
+            ViewBag.ProjectID = id.Value;
+            return View(f);
         }
 
-        public ActionResult CreateFile()
+        public ActionResult CreateFile(int? id)
         {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            int projectId = id.Value;
             string userId = User.Identity.GetUserId();
-            FileService.Instance.AddNewFile(userId);
-            return RedirectToAction("Index", "Myfiles");
+            FileService.Instance.AddNewFile(userId, projectId);
+            return RedirectToAction("Index", "Myfiles", new { id = projectId });
+
+           // return View();
         }
     }
 }
