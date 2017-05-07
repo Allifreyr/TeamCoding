db4f5d4e98a7a31140cbad5f86dfe4928240c7f4
hjordisj16 <hjordisj16@ru.is>
2017-05-07 16:27:52 +0000

    Create file síða útfærð

--- TeamCode/Controllers/MyFilesController.cs
+++ TeamCode/Controllers/MyFilesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamCode.Services;
+using TeamCode.Models.Entities;
+using Microsoft.AspNet.Identity;
 
 namespace TeamCode.Controllers
 {
@@ -10,14 +13,17 @@ namespace TeamCode.Controllers
     {
         
         // GET: MyFiles
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Create()
+        public ActionResult CreateFile()
         {
-            return View();
+            string userId = User.Identity.GetUserId();
+            FileService.Instance.AddNewFile(userId);
+            return RedirectToAction("Index", "Myfiles");
         }
     }
 }
--- TeamCode/Services/FileService.cs
+++ TeamCode/Services/FileService.cs
@@ -9,11 +9,27 @@ namespace TeamCode.Services
 {
     public class FileService
     {
+        private static FileService instance;
+
+        public static FileService Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new FileService();
+                return instance;
+            }
+        }
         private ApplicationDbContext _db;
 
         public FileService()
         {
             _db = new ApplicationDbContext();
+        }
+
+        internal File AddNewFile()
+        {
+            throw new NotImplementedException();
         }
 
         public List<File> GetFilesByProject(int projectID)
@@ -31,9 +47,19 @@ namespace TeamCode.Services
 
         public List<File> GetFileByID(int fileID)
         {
-            var fileListByID = (from p in _db.Files where p.id == fileID select p).ToList();
+            var fileListByID = (from f in _db.Files where f.id == fileID select f).ToList();
 
             return fileListByID;
+        }
+        public void AddNewFile(string userId)
+        {
+            File file = new File();
+            file.fileName = "Untitled";
+            file.user = (from u in _db.Users
+                            where u.Id == userId
+                            select u).SingleOrDefault();
+            _db.Files.Add(file);
+            _db.SaveChanges();
         }
     }
 }
