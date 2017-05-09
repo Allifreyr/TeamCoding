d7e2865a60bf0771ce6764b095ba1f8a906dcb27
hjordisj16 <hjordisj16@ru.is>
2017-05-09 23:40:42 +0000

    User to project team work

---
+++ TeamCode/Models/ViewModels/UserToProjectsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamCode.Models.ViewModels
+{
+    public class UserToProjectsViewModel
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string userId { get; set; }
+        public int projectId { get; set; }
+
+        
+    }
+}
